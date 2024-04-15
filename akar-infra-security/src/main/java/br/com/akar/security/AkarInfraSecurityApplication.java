f626cc405e2875c5a2b9237333fa99a1078aa5ee
Alexandre Visnardi <avisnardi@gmail.com>
2024-04-15 12:29:26 -0300

    commit faltante

---
+++ akar-infra-security/src/main/java/br/com/akar/security/AkarInfraSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.akar.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkarInfraSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkarInfraSecurityApplication.class, args);
+	}
+
+}
